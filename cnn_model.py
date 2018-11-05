9a13017e47ff67490b1d309bb82e185ecaf8f2c8
Zhang Jialu <zhangjloo1@163.com>
2018-11-05 18:02:38 +0800

    使用多个尺寸的卷积核，其他参数不变，未使用时验证集F1为0.73，使用后可到0.75。

--- cnn_model.py
+++ cnn_model.py
@@ -9,7 +9,7 @@ class TCNNConfig(object):
     seq_length = 4000  # 序列长度
     num_classes = 19  # 类别数
     num_filters = 512  # 卷积核数目
-    kernel_size = 5  # 卷积核尺寸
+    kernel_sizes = [2, 3, 5]  # 卷积核尺寸
     vocab_size = 10000  # 词汇表大小，修改此参数需要。。。。。。。。。
 
     hidden_dim = 1024  # 全连接层神经元
@@ -46,20 +46,28 @@ class TextCNN(object):
             # tf.nn.embedding_lookup返回一个三维张量[batch_size, seq_length, embedding_dim]
             embedding_inputs = tf.nn.embedding_lookup(embedding, self.input_x)
 
-        with tf.name_scope("cnn"):
-            # CNN layer
-            # tf.layers.conv1d除了输入数据只有两个必要参数：滤波器数量和卷积核的长度，卷积核的宽与词向量的维度一样
-            # 返回一个三维张量[batch_size, seq_length-kernel_size+1, num_filters]
-            conv = tf.layers.conv1d(embedding_inputs, self.config.num_filters, self.config.kernel_size, name='conv')
-            # global max pooling layer
-            # 由于reduction_indices=[1]，是在seq_length-kernel_size+1的维度上取最大值
-            # 返回一个二维张量[batch_size, num_filters]
-            gmp = tf.reduce_max(conv, reduction_indices=[1], name='gmp')
+        pooled_outputs = []
+        for kernel_size in self.config.kernel_sizes:
+            with tf.name_scope("cnn-maxpool-%s" % kernel_size):
+                # 卷积层
+                # tf.layers.conv1d除了输入数据只有两个必要参数：滤波器数量和卷积核的长度，卷积核的宽与词向量的维度一样
+                # 返回一个三维张量[batch_size, seq_length-kernel_size+1, num_filters]
+                conv = tf.layers.conv1d(embedding_inputs, self.config.num_filters, kernel_size, name='conv-%s' % kernel_size)
+                # 有人在这里加一层relu激活
+                # conv = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # 最大池化层
+                # 由于reduction_indices=[1]，是在seq_length-kernel_size+1的维度上取最大值
+                # 返回一个二维张量[batch_size, num_filters]
+                gmp = tf.reduce_max(conv, reduction_indices=[1], name='gmp-%s' % kernel_size)
+                pooled_outputs.append(gmp)
+
+        # 拼接每种滤波器卷积以及池化之后的结果
+        self.h_pool = tf.concat(pooled_outputs, 1)
 
         with tf.name_scope("score"):
             # 全连接层，后面接dropout以及relu激活
             # tf.layers.dense返回二维张量[batch_size, hidden_dim]
-            fc = tf.layers.dense(gmp, self.config.hidden_dim, name='fc1')
+            fc = tf.layers.dense(self.h_pool, self.config.hidden_dim, name='fc1')
             fc = tf.contrib.layers.dropout(fc, self.keep_prob)
             fc = tf.nn.relu(fc) # 返回还是二维张量[batch_size, hidden_dim]
 
@@ -67,7 +75,7 @@ class TextCNN(object):
             # 还是全连接层，返回二维张量[batch_size, num_classes]
             self.logits = tf.layers.dense(fc, self.config.num_classes, name='fc2')
             # 预测类别，最大的下标就是预测类别，返回一维张量维度为batch_size
-            self.y_pred_cls = tf.argmax(tf.nn.softmax(self.logits), 1)
+            self.y_pred_cls = tf.argmax(self.logits, 1)
 
         with tf.name_scope("optimize"):
             # 损失函数，交叉熵
